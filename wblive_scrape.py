89279763b4a83b17581ba950c0b2d614d813c21e
markliammurphy <liam.murphy549@gmail.com>
2017-12-30 14:15:41 -0500

    beautiful soup update

--- wblive_scrape.py
+++ wblive_scrape.py
@@ -3,6 +3,24 @@ Scrape surf reports from wblive, save result as a csv
 '''
 
 import argparse
+from bs4 import BeautifulSoup
+import urllib
+import requests
+
+
+def get_data():
+    pass
+
+
+def crawl():
+    archive = 'http://www.wblivesurf.com/reports/'
+    r = requests.get(archive, auth=('user', 'pass'))
+
+    soup = BeautifulSoup(r.text, "html5lib")
+
+    # find the dates we want
+    for date in soup.find_all("div", class_="postDate"):
+        print(date.text)
 
 
 if __name__ == '__main__':
@@ -15,3 +33,5 @@ if __name__ == '__main__':
                         type=str,
                         default='12/31/16',
                         help='Start date in form mm/dd/yy')
+    ARGS, _ = parser.parse_known_args()
+    crawl()
